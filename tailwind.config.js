841c3d48b72dd5e185963364f4b067d60a171915
Omar <km@kms-MacBook-Pro.local>
2025-01-13 02:48:29 +0300

    Setup project with Vue 3, Vite, Tailwind CSS, and basic configuration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}'
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          primary: '#5C2D91',// Purple
+          secondary: '#F3F4F6',
+          accent: '#E0E7FF',
+          darkBg: '#1F2937',
+          darkText: '#D1D5DB',
+        }
+      }
+    },
+    plugins: [],
+  }
+  
